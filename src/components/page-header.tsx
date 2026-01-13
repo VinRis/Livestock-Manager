eef1a2abe6db85fabe91b7a02af94a3a89500489
Kevin Kariuki <kevinkaris47@gmail.com>
2026-01-13 19:14:59 +0000

    This button should be flexible, so that the words inside do not get cut

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -8,12 +8,12 @@ type PageHeaderProps = {
 
 export function PageHeader({ title, description, children }: PageHeaderProps) {
   return (
-    <header className="flex items-center gap-4 px-4 pt-4 sm:px-6 sm:pt-6">
+    <header className="flex flex-wrap items-center justify-between gap-4 px-4 pt-4 sm:px-6 sm:pt-6">
       <div className="flex-1">
         <h1 className="text-2xl font-bold tracking-tight font-headline">{title}</h1>
         {description && <p className="text-muted-foreground">{description}</p>}
       </div>
-      {children && <div className="flex items-center gap-2">{children}</div>}
+      {children && <div className="flex flex-shrink-0 items-center gap-2">{children}</div>}
     </header>
   );
 }
