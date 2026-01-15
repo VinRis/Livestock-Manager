3262a2da93edc22ecdd00a9d37d1ebd824f2c751
Kevin Kariuki <kevinkaris47@gmail.com>
2026-01-15 09:59:13 +0000

    The FAB should be above the navigation bar when viewing on larger screen

--- src/app/(app)/activity/page.tsx
+++ src/app/(app)/activity/page.tsx
@@ -233,7 +233,7 @@ export default function ActivityLogPage() {
       {/* Add FAB */}
        <Dialog open={isAddDialogOpen} onOpenChange={setAddDialogOpen}>
           <DialogTrigger asChild>
-            <Button className="absolute bottom-20 right-4 h-14 w-14 rounded-full shadow-lg sm:bottom-6">
+            <Button className="absolute bottom-20 right-4 h-14 w-14 rounded-full shadow-lg sm:bottom-20">
               <PlusCircle className="h-6 w-6" />
               <span className="sr-only">Log Activity</span>
             </Button>
