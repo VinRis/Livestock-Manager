568ef1eb1db1e1e18dfec5fb72ceb3bb1b368e7a
Kevin Kariuki <kevinkaris47@gmail.com>
2026-01-20 18:55:56 +0000

    When viewing on a mobile, when the values are huge. The page size grows

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <div className="relative flex flex-col w-full min-h-screen md:pb-16 pb-16">
+      <div className="relative flex flex-col w-full min-h-screen overflow-x-hidden md:pb-16 pb-16">
         {children}
       </div>
       <MobileNav />
