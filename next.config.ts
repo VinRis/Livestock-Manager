f17a8e2fc298df52d20c215575ae82dfa5c0e5fc
Kevin Kariuki <kevinkaris47@gmail.com>
2026-01-20 18:25:37 +0000

    Let us add some neat shortcuts, when the user long presses the app icon

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 import withPWA from '@ducanh2912/next-pwa';
 
@@ -39,4 +40,32 @@ export default withPWA({
   register: true,
   scope: '/',
   sw: 'service-worker.js',
+  manifest: {
+    name: 'Livestock Lynx',
+    short_name: 'Lynx',
+    description: 'Manage your farm with ease.',
+    shortcuts: [
+      {
+        name: 'Log New Activity',
+        url: '/activity',
+        description: 'Quickly log a new farm activity',
+      },
+      {
+        name: 'Add New Task',
+        url: '/tasks',
+        description: 'Add a new task to your schedule',
+      },
+      {
+        name: 'Record Transaction',
+        url: '/finance',
+        description: 'Record a new income or expense item',
+      },
+       {
+        name: 'View Livestock',
+        url: '/livestock',
+        description: 'View your livestock categories',
+      },
+    ],
+  },
 })(nextConfig);
+
